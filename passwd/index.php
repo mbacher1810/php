b8812aff0d38b27c1229f8109585903050f312e5
Michael Bacher <mbacher@schueler.hakzell.at>
2023-02-23 12:15:45 +0100

    update

--- passwd/index.php
+++ passwd/index.php
@@ -6,4 +6,18 @@ require 'vendor/autoload.php';
 
 $pass = new Password();
 $pass->addRandomNumber();
+$pass->setRandomStringAmount(5);
+$pass->addRandomString();
 echo $pass->getValue();
+?>
+<br/>
+<?php
+$pass2 = new Password();
+echo $pass2 ->addRandomNumber()
+            ->setRandomStringAmount(6)
+            ->addRandomString()
+            ->getValue();
+?>
+<br/>
+<?php
+echo (new Password())->addRandomString()->getValue();
--- passwd/src/Password.php
+++ passwd/src/Password.php
@@ -3,16 +3,44 @@ namespace Mbacher\Passwd;
 class Password
 {
     private string $password;
+    private int $amountChars = 2;
+
     public function __construct()
     {
         $this->password = 'geheim';
+    }
+
+    public function setRandomStringAmount(int $amount):Password
+    {
+        $this->amountChars = $amount;
+
+        // eigenes Objekt wird zurückgegeben
+        return $this;
     }
     public function getValue(): string
     {
         return $this->password;
     }
-    public function addRandomNumber(){
+    public function addRandomNumber():Password{
         $rand = mt_rand(1,10);
         $this->password = $this->password . $rand;
+
+        return $this;
+    }
+
+    public function addRandomString():Password
+    {
+        $zk = 'abcdefghijklmnopqrstuvwxyz';
+        $zk = $zk . strtoupper($zk);
+
+        // je nach der Angabe in amountChars werden Zeichen von a-z/A-Z hinzugefügt
+        for ($i=0;$i < $this->amountChars;$i++) {
+            $zz = mt_rand(0, strlen($zk) - 1);
+
+            $this->password = $this->password . $zk[$zz];
+        }
+
+        return $this;
+
     }
 }
